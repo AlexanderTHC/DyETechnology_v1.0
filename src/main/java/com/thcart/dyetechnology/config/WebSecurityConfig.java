f7e5e5c7576761901d014f098d8650c03bccda30
Z4X <sosaalexander.joa@gmail.com>
2022-08-19 10:35:56 -0300

    ?

--- src/main/java/com/thcart/dyetechnology/config/WebSecurityConfig.java
+++ src/main/java/com/thcart/dyetechnology/config/WebSecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  *
@@ -24,7 +26,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    //PERMITE INGRESAR Y VISUALIZAR LAS IMAGENES QUE SE ENCUENTRAN DENTRO DE ESE DIRECTORIO EN EL HOME
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").addResourceLocations("file:img/");
+    }
     
     //INYECTAR EL ORIGEN DE DATOS LA CONEXIÓN A LA DB
     @Autowired
@@ -71,72 +79,3 @@ public class WebSecurityConfig {
    } 
    
 }
-
-
-
-//import javax.sql.DataSource;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-///**
-// *
-// * @author joaqu
-// */
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-//public class WebSecurityConfig{
-//    
-//    //INYECTAR EL ORIGEN DE DATOS LA CONEXIÓN A LA DB
-//    @Autowired
-//    DataSource dataSource;
-//    
-//    @Bean //CADA OBJETO DE JAVA EN UN BEAN, ES PARA QUE CREE AUTOMATICAMENTE ESE METODO Y CREA LA INSTANCIA DEL INCRIPTADOR
-//    BCryptPasswordEncoder getEnconder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeHttpRequests((authz) -> authz
-//                .anyRequest().authenticated()
-//            );
-//        return http.build();
-//    }
-//
-//  /*  @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/layout/**", "/home/**", "/**", "/css/**", "/js/**", "/img/**")
-//                .permitAll().anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/login").permitAll()
-//                .and()
-//                .logout().permitAll()
-//                .and().exceptionHandling().accessDeniedPage("/error_403")
-//                        .and().csrf().disable().cors();
-//        
-//    } */
-//    
-//   @Autowired
-//   public void configureGlobal(AuthenticationManagerBuilder build) throws Exception{
-//       
-//       //AUTENTICACIÓN CON JDBC:
-//       build.jdbcAuthentication()
-//               .dataSource(dataSource)
-//               .usersByUsernameQuery("select nombre, clave, activo from usuarios where nombre = ?")
-//               .authoritiesByUsernameQuery("select u.nombre, r.nombre from roles r "
-//               + "inner join usuarios u on "
-//               + "u.id_rol = r.id where u.nombre = ?");
-//   } 
-//   
-//}
--- src/main/java/com/thcart/dyetechnology/controller/RegisterController.java
+++ src/main/java/com/thcart/dyetechnology/controller/RegisterController.java
@@ -1,64 +1,13 @@
 package com.thcart.dyetechnology.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.thcart.dyetechnology.model.entities.Usuario;
-import com.thcart.dyetechnology.model.service.IRolService;
-import com.thcart.dyetechnology.model.service.IUsuarioService;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/registrarse")
 public class RegisterController {
 
-    @Autowired
-    IUsuarioService userService;
-    @Autowired
-    IRolService rolService;
-    @Autowired
-    private PasswordEncoder encoder;
 
-    @GetMapping()
-    public String agregarUsuarios(Model model){
-        
-    Usuario usuario = new Usuario();   
-    
-    model.addAttribute("subtitulo", "Nuevo Cajero");
-    
-    model.addAttribute("usuario", usuario);
-    
-    model.addAttribute("roles", rolService.buscarTodos());
-        
-    return "registrarse/formRegister";
-    }
-
-
-    @PostMapping("/nuevo")
-    public String nuevoUser(@Valid Usuario usuario, BindingResult result, Model model, RedirectAttributes redirect){
-
-        if(result.hasErrors()){
-            
-            model.addAttribute("subtitulo", "Corrija los Errores");
-          
-            model.addAttribute("danger", "¡Datos erróneos!");
-            
-            return "register";
-        }
-
-        usuario.setClave(encoder.encode(usuario.getClave()));
-        usuario.setRol(rolService.buscarPorId((long) 3));
-       
-       userService.guardar(usuario);
-
-        return "redirect:/";
-    }
 }
