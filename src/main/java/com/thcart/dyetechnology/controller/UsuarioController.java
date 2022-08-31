acbfe48a05146a1b2d384d76cc18b09e43e8ab5c
5l1a4 <eliandan090@gmail.com>
2022-08-31 15:44:37 -0300

    Fix: arreglar boton de editar datso

--- src/main/java/com/thcart/dyetechnology/controller/UsuarioController.java
+++ src/main/java/com/thcart/dyetechnology/controller/UsuarioController.java
@@ -11,12 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
  * @author Micholini
  */
 @Controller
+@SessionAttributes({"usuario"})
 public class UsuarioController {
         
     @Autowired
@@ -32,5 +37,17 @@ public class UsuarioController {
 
         return"usuario/perfil";
     }
+    
+    @GetMapping("/editar/{id}")
+    public String editarPerfil(@PathVariable("id") long id, 
+            Model model, RedirectAttributes redirect){
+        
+            Usuario usuario = (Usuario) usuarioService.buscarPorId(id);
+            
+            model.addAttribute("usuario", usuario);
+    
+        return"usuario/form";
+    }
+
     
 }
