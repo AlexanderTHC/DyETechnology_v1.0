ec22f6482ee657136adcbf5fb35f2b81299eb77e
z4x <sosaalexander.joa@gmail.com>
2023-02-14 16:22:04 -0300

    Se agrego un Inicio de visualización para los Administradores

---
+++ src/main/java/com/thcart/dyetechnology/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.thcart.dyetechnology.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class AdminController {
+    
+
+    @GetMapping("admin/inicio")
+    public String inicio(Model model) {
+
+        model.addAttribute("titulo", "DyE Technology - Administración");
+        model.addAttribute("subtitulo", "Bienvenido a DyE Technology - Administración");
+
+        return "administrador/inicio";
+    }
+    
+}
---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    $('.counter-value').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 3500,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
